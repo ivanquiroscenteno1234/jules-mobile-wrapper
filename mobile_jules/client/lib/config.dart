65a2be3026e3d70de936ff92cacde27e222c167b
ivanquiroscenteno1234 <ivan_ijnavi@hotmail.com>
2025-12-26 17:46:10 -0600

    chore: update server URL to use ngrok for mobile device testing

--- mobile_jules/client/lib/config.dart
+++ mobile_jules/client/lib/config.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 class AppConfig {
-  // Use localhost for web, 10.0.2.2 for Android emulator
-  static String serverUrl = kIsWeb ? 'http://localhost:8000' : 'http://10.0.2.2:8000';
+  // Server URL - using ngrok for remote access
+  static String serverUrl = 'https://jada-volatilisable-fiendishly.ngrok-free.dev';
   
   // Auto mode: auto-approve plans and create PRs
   static bool autoMode = false;
